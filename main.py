4483a2b9571977ca6ce329f6b1012908e3766cd7
Momina <student.165557@worcesterschools.net>
2026-04-30 13:40:15 -0400

    Book Nook

---
+++ main.py
@@ -0,0 +1,27 @@
+# make list of books
+My_Books = []
+# print Welcome message and image 
+print("Welcome to the book nook.")
+    # image
+# print out the commands
+print("To add (add)")
+print("To show (show)")
+print("To remove (remove)")
+print("To count (count)")
+print("To quit (quit)")
+# ask user for what they wanna do (While Loop)
+# Add
+    # ask for book 
+    # add to list 
+    # success/failure message
+# Show 
+    # print out list with numbers
+# Remove 
+    # ask for book
+    # remove the book
+    # success/failure message 
+# Count 
+    # print out the message with count number
+# Quit
+    # stop 
+    # end program and loop
