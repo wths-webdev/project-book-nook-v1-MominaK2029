e3f50db7d52e20538953cc7ccde9592a9ca03f0f
Momina <student.165557@worcesterschools.net>
2026-05-12 13:30:38 -0400

    Book Nook

--- main.py
+++ main.py
@@ -20,36 +20,41 @@ print("To quit (q)")
 # ask user for what they wanna do (While Loop)
 
 while True:
-    user_input = input("What would you like to do?")    
+    user_input = input("What would you like to do?").lower().strip()                  
     # Add
         # ask for book 
-    if user_input.lower() == "add":
+    if user_input == "add":
         book_name = input("Which book would you like to add?")
+        (book_name.isalpha())
         # add to list 
-        My_Books.append(book_name.isalpha())
+        My_Books.append(book_name)
         # success message
         print("Book has been successfully added.")
 
     # Show 
         # print out list with numbers
-    elif user_input.lower() == "show":
+    elif user_input == "show":
         for count, item in enumerate (My_Books, start=1):
             print(f"{count}. {item}")
     # Remove 
-    elif user_input.lower() == "remove":
+    elif user_input == "remove":
         # ask for book
         book_name2 = input("Which book would you like to remove?")
+        (book_name2.isalpha())
         # remove the book
-        My_Books.remove(book_name2.isalpha())
+        My_Books.remove(book_name2)
         # success message
         print("Book has been successfully removed.") 
+        # Book remove error message
+        if book_name2 not in My_Books:
+            print("Book is not avaliable in Book Nook.")
     # Count 
-    elif user_input.lower() == "count":
+    elif user_input == "count":
         # print out the message with count number
         print(len(My_Books)) 
         print("book(s)")
     # Quit
     else:
-        user_input.lower() == ("quit")
+        user_input == ("quit")
         print("Goodbye, thanks for visiting the Book Nook!")
         break
